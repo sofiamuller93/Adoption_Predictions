661f71fd2b33b75e78dead382c4ece8691280603
Sofia Muller <sofia.muller@hotmail.com>
2018-10-02 19:34:26 -0500

    fix python app

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -87,12 +87,3 @@ def upload_file():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-@app.route("/resources")
-def index():
-    """Return the homepage."""
-    return render_template("resources_intakes.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
